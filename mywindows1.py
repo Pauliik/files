0dfa8bc0b5565e41573ab705926cd4e939781294
Pauliik <145107263+Pauliik@users.noreply.github.com>
2023-12-26 15:34:27 +0300

    Add files via upload

---
+++ mywindows1.py
@@ -0,0 +1,45 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+
+import sys
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+
+        self.setWindowTitle("Простая программа")
+        self.setGeometry(300, 250, 350, 200)
+
+        self.new_text = QtWidgets.QLabel(self)
+
+        self.main_text = QtWidgets.QLabel(self)
+        self.main_text.setText("Это базовая надпись")
+        self.main_text.move(100, 100)
+        self.main_text.adjustSize()
+
+        self.btn = QtWidgets.QPushButton(self)
+        self.btn.move(70, 150)
+        self.btn.setText("Нажми на меня")
+        self.btn.setFixedWidth(200)
+        self.btn.clicked.connect(self.add_labal)
+
+
+
+
+    def add_labal(self):
+        self.new_text.setText("Вторая надпись")
+        self.new_text.move(100, 50)
+        self.new_text.adjustSize()
+
+
+
+def application():
+    app = QApplication(sys.argv)
+    window = Window()    
+
+    window.show()
+    sys.exit(app.exec_())
+
+application()
